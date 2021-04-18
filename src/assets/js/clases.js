9a44a594a23582e0fab4209d520cfc846f292940
Victor Daniel Franco Valencia <vfrancov25@gmail.com>
2021-04-18 12:09:17 -0500

    Calculadora agreagada

--- src/assets/js/clases.js
+++ src/assets/js/clases.js
@@ -14,12 +14,33 @@ class MathOperations {
     }
 
     division(numberOne, numberTwo) {
-        return numberOne / numberTwo;
+        if (numberTwo === 0)
+            return 0;
+        else
+            return numberOne / numberTwo;
     }
 }
 
 const operations = new MathOperations();
 
+console.log('MathOperations');
 console.log(operations.addition(2, 3));
 console.log(operations.addition(3, 4));
-console.log(operations.addition(6, 9));
+console.log(operations.division(6, 0));
+
+class Calculadora {
+    operations;
+
+    constructor(operations) {
+        this.operations = operations;
+    }
+
+    total(subtotal, total) {
+        return this.operations.addition(subtotal, total);
+    }
+}
+
+console.log('Calculadora');
+const calculator = new Calculadora(new MathOperations);
+
+console.log(calculator.total(5, 6));
