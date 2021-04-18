69cb82ba374da0c776ff7522925802eb6ea77873
Victor Daniel Franco Valencia <vfrancov25@gmail.com>
2021-04-18 11:23:04 -0500

    cambio en funciones

--- src/assets/js/funciones.js
+++ src/assets/js/funciones.js
@@ -1,45 +1,45 @@
+'use-strict';
+//SOLID
 
-function sumar (...items) {
-    let resultado = 0;
+// SRP : Single Responsibility Principle
+// Funciones : Una funcion debe hacer una cosa, debe hacerla bien, y debe ser lo unico que haga
 
-  items.forEach(function(n){
-      resultado+=n;
-  });
+/**
+ * Suma dos numeros enteros pasados como arguments a, b
+ * @param {*} a 
+ * @param {*} b 
+ * @returns resultado de la suma de los 2 enteros
+ */
+function sumar(a, b) {
+  return (a + b);
+}
+
+function restar(a, b) {
+  return (a - b);
+}
+
+function multiplicar(a, b) {
+  return (a * b);
+}
+
+function dividir(a, b) {
+
+  if (b === 0)
+    throw "No es posible dividir entre 0";
+  else {
+    resultado = (a / b);
+  }
 
   return resultado;
 }
 
-function restar (a,b,...items) {
-    let resultado = a-b;
+console.log(sumar(10, 5));
+console.log(restar(10, 5));
 
-  items.forEach(function(n){
-      resultado-=n;
-  });
-
-  return resultado;
+try {
+  console.log(dividir(10, 2));
+} catch (e) {
+  console.error(e);
 }
 
-function multiplicar (a,b,...items) {
-    let resultado = a*b;
-
-  items.forEach(function(n){
-      resultado*=n;
-  });
-
-  return resultado;
-}
-
-function dividir (a,b,...items) {
-    let resultado = a/b;
-
-  items.forEach(function(n){
-      resultado/=n;
-  });
-
-  return resultado;
-}
-
-console.log(sumar(10,5));
-console.log(restar(10,5,2,1));
-console.log(dividir(10,5,1));
-console.log(multiplicar(10,5,5));
+console.log(multiplicar(10, 5));
